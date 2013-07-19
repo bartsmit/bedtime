5279897692981bd1552133c3a320f4a85fe89663
bartsmit <bart@smits.co.uk>
2013-07-19 17:11:22 +0100

    edit sleep.php

--- sleep.php
+++ sleep.php
@@ -1,3 +1,6 @@
+<?php
+include "dbconnect.php";
+?>
 <html><head><title>Time to go to sleep</title>
 <meta http-equiv="cache-control" content="max-age=0" />
 <meta http-equiv="cache-control" content="no-cache" />
