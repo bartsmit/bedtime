442b77ab9af39005c72b6d6ffade24b6493878fc
root <root@ukcrlcscin0032.tcllab.co.uk>
2013-07-29 16:14:19 +0100

    tidied the database perl

--- get_manlst.pl
+++ get_manlst.pl
@@ -2,32 +2,17 @@
 
 use DBI;
 use LWP::Simple;
+use BedtimeDB;
 use strict;
 
-open (CONF,'/etc/bedtime.conf') or die "Cannot open configuration file - $!\n";
-# Read the conf file into an array and filter out all lines consisting of just white space and comments
-my @conf = <CONF>; close CONF;
-@conf = grep (!/^\s*$/,@conf);
-@conf = grep (!/^#/,@conf);
-
-# Read the remaining lines into a hash split on =
-my %vals;
-foreach (@conf) {
-   chomp;
-   my @pair = split(/\s*=\s*/);
-   $vals{$pair[0]}=$pair[1];
-}
-
-# Collect the credentials and connect to the database
-my $user = $vals{'dbuser'};
-my $pass = $vals{'dbupass'};
-my $dbis = "DBI:mysql:".$vals{'dbname'}.":".$vals{'dbhost'};
-my $dbh = DBI->connect($dbis,$user,$pass) or die "Cannot connect to database $dbis with user $user - $!\n";
+# Connect to the database
+my $dbh = &BedtimeDB::dbconn;
 
 # Download the manufacturers list from Wireshark and use only the lines starting with three MAC bytes
 my @man = split(/\n/,get("http://anonsvn.wireshark.org/wireshark/trunk/manuf"));
 my @lnman = grep(/\/36/,@man);
 @man = grep(/^(([A-F0-9]){2}[-:]){2}([A-F0-9]){2}\s+/,@man);
+
 # Take the long MAC's and trim them
 foreach(@lnman) {
    s/0:00\/36//;
