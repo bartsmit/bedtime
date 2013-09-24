b0ad9e527fbc9799c91e2ce3a4854458398116b1
bartsmit <bart@smits.co.uk>
2013-09-24 17:19:53 +0100

    typo in postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -8,7 +8,7 @@ setsebool -P httpd_can_network_connect 1
 iptables -I INPUT 4 -m state --state NEW -p tcp -m tcp --dport 80 -j ACCEPT
 iptables -I INPUT 4 -m state --state NEW -p tcp -m tcp --dport 3128 -j ACCEPT
 iptables -I INPUT 4 -m state --state NEW -p udp -m udp --dport 5353 -j ACCEPT
-iptables -I INPUT 4 -m state --stete NEW -p udp -m udp --dport 67:68 -j ACCEPT
+iptables -I INPUT 4 -m state --state NEW -p udp -m udp --dport 67:68 -j ACCEPT
 /etc/init.d/iptables save
 
 ip6tables -I INPUT 4 -m state --state NEW -p tcp -m tcp --dport 80 -j ACCEPT
@@ -19,11 +19,18 @@ ip6tables -I INPUT 4 -m state --state NEW -p udp -m udp --dport 5353 -j ACCEPT
 /etc/init.d/mysqld start && chkconfig mysqld on
 mysql < /usr/share/bedtime/create.sql
 
-/etc/init.d/httpd start && chkconfig httpd on
+mv /etc/squid/squid.conf /etc/squid/squid.conf.rpmsave
+cp /usr/share/bedtime/squid.conf /etc/squid
 /etc/init.d/squid start && chkconfig squid on
+
+/etc/init.d/httpd start && chkconfig httpd on
 /etc/init.d/ntpd start && chkconfig ntpd on
 /etc/init.d/messagebus start && chkconfig messagebus on
 /etc/init.d/avahi-daemon start && chkconfig avahi-daemon on
+
+cp /usr/share/bedtime/bedtime /etc/init.d
+chmod 755 /etc/init.d/bedtime
+/etc/init.d/bedtime start && chkconfig bedtime on
 
 touch /usr/share/bedtime/network
 chown apache.root /usr/share/bedtime/network
