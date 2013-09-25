6d971e6539275b001d2acf1caece4e07510f93e9
bartsmit <bart@smits.co.uk>
2013-09-25 10:18:10 +0100

    De-duped files

--- usr/share/bedtime/etc/cron.hourly/bedtime.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/usr/share/bedtime/bin/update_ipt
-/usr/share/bedtime/bin/dhcp_slurp
--- usr/share/bedtime/var/www/html/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("/bedtime/dbconn.php");
-if ($mysqli->connect_errno) {
-   header ("Location: /install/");
-} else {
-   header ("Location: /bedtime/");
-}
-?>
