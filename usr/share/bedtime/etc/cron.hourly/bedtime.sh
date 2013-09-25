4a001f6711c82ba18852b1f9bd36e53f8bf038c1
bartsmit <bart@smits.co.uk>
2013-09-25 10:14:48 +0100

    Shuffled files for easier RPM build

---
+++ usr/share/bedtime/etc/cron.hourly/bedtime.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/usr/share/bedtime/bin/update_ipt
+/usr/share/bedtime/bin/dhcp_slurp
--- usr/share/bedtime/install/index.php
+++ usr/share/bedtime/install/index.php
@@ -18,7 +18,7 @@ if (isset($_GET['sqlrootpw'])) {
       $mysqli->query("delete from mysql.user where user='sleepy'");
       $mysqli->query("grant all on bedtime.* to 'sleepy'@'localhost' identified by '$pass'");
       $mysqli->query("flush privileges");
-      $mysqli->query("replace into bedtime.settings (variable,value) values('dns','".$_GET['dns']."')";
+      $mysqli->query("replace into bedtime.settings (variable,value) values('dns','".$_GET['dns']."')");
       # Run btsetup with the new password
       $sock = socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
       $result = socket_connect($sock,'127.0.0.1',5000);
