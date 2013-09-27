848bbe8e7c0765612d7605d4577fb9bb296bacbe
bartsmit <bart@smits.co.uk>
2013-09-27 10:43:31 +0100

    Misplaced finish.php

---
+++ usr/share/bedtime/website/install/finish.php
@@ -0,0 +1,14 @@
+<?php
+$net = file('/usr/share/bedtime/network');
+foreach ($net as $line) {
+   $bits = preg_split ("/\//",rtrim($line));
+   if ($bits[1] === "dhcp") {
+      $dhcp = $bits[0];
+   }
+}
+print "<html><title>Finish Install</title><body>\n";
+print "Congratulations, Bedtime is set up.<br><br>Next, you will need to disable the DHCP server<br>\n";
+print "Click <a href=\"http://$dhcp\" target=\"_blank\">here</a> to manage your router in a new tab/window<br><br>\n";
+print "Once the DHCP service has been switched off, click <a href=\"/bedtime\">here</a><br>\n";
+print "and log in with name admin and password admin</body></html>\n";
+?>
